4bf02a5a15197b156c812b1ad9e4acfc69c89ae6
MaximeSaliou72 <maximesaliou72@gmail.com>
2024-01-15 12:03:06 +0100

    exercice 1 dotenv

---
+++ exercices/exercice-1-message-dotenv/Message.js
@@ -0,0 +1,31 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+export default class Message {
+    constructor() {
+        this._lang = process.env.LANGUAGE;
+        this._translates = {
+            'fr': 'Bonjour tout le monde!',
+            'en': 'Hello World!'
+        };
+        this._array = [];
+        console.log(process.env.LANGUAGE)
+    }
+
+    get() {
+        return this._translates[this._lang];
+    }
+
+    setLang(lang) {
+        this._lang = lang;
+    }
+
+    getArray() {
+        return this._array;
+    }
+
+    add(number) {
+        this._array.push(number);
+    }
+}
---
+++ skeleton-nodejs/index.js
@@ -0,0 +1,5 @@
+import Message from "../exercices/exercice-1-message-dotenv/Message.js"
+
+let message = new Message()
+
+console.log(message.get())
