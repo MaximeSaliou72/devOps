5b90812da9262e7e6d3f72224041bb51c8b06feb
MaximeSaliou72 <maximesaliou72@gmail.com>
2024-01-15 16:55:48 +0100

    password 2

--- exercices/exercice-7-password-validation/__tests__/index_test.js
+++ exercices/exercice-7-password-validation/__tests__/index_test.js
@@ -7,7 +7,12 @@ test("Le mot de passe doit comporter au moins 8 caractères", () => {
   );
 });
 
+test("Le mot de passe doit contenir au moins 2 chiffres", () => {
+    const password = new Password();
+    expect(password.passWodrLength("qwertyuiop2")).toBe("Le mot de passe doit contenir au moins 2 chiffres");
+});
+
 test("Mot de passe valide", () => {
   const password = new Password();
-  expect(password.passWodrLength("qwertyuiop")).toBe("Mot de passe valide");
+  expect(password.passWodrLength("qwertyuiop22")).toBe("Mot de passe valide");
 });
--- exercices/exercice-7-password-validation/src/password.js
+++ exercices/exercice-7-password-validation/src/password.js
@@ -6,6 +6,13 @@ module.exports = class Password {
     if (password.length < 8) {
       return "Le mot de passe doit comporter au moins 8 caractères";
     }
+
+    // Supprime tous les caractères qui ne sont pas des chiffres
+    const numberCount = password.replace(/[^0-9]/g, "").length;
+    if (numberCount < 2) {
+      return "Le mot de passe doit contenir au moins 2 chiffres";
+    }
+  
     return "Mot de passe valide";
   }
 
