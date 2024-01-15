4dd7d89ee196185f89e26ad96e5e899f7cad0f2f
MaximeSaliou72 <maximesaliou72@gmail.com>
2024-01-15 14:30:34 +0100

    fizBuzz multiple

---
+++ exercices/exercice-5-fizzbuzz/__tests__/index_test.js
@@ -0,0 +1,20 @@
+let FizzBuzz = require("../src/fizzBuzz.js");
+
+test('retourne FizzBuz si le resultat est un multiple de 3 et 5', () => {
+    const fizzBuzz = new FizzBuzz();
+    expect(fizzBuzz.getMultiple(15)).toBe('FizzBuzz');
+    expect(fizzBuzz.getMultiple(30)).toBe('FizzBuzz');
+});
+
+test('retourne Fizz si le resultat est un multiple de 3', () => {
+    const fizzBuzz = new FizzBuzz();
+    expect(fizzBuzz.getMultiple(3)).toBe('Fizz');
+    expect(fizzBuzz.getMultiple(9)).toBe('Fizz');
+});
+
+
+test('retourne Buzz si le resultat est un multiple de 5', () => {
+    const fizzBuzz = new FizzBuzz();
+    expect(fizzBuzz.getMultiple(5)).toBe('Buzz');
+    expect(fizzBuzz.getMultiple(10)).toBe('Buzz');
+});
